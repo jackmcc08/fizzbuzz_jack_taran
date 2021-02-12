79a89fe613b2c92397be9d4f3e7b3e676434c540
Jack McCarthy <jackmcc08@gmail.com>
2021-02-12 16:52:51 +0000

    Figured out alternate fizzbuzz program and wrote up testing suite - all works - final commit for now

---
+++ lib/fizzbuzz_alt.rb
@@ -0,0 +1,18 @@
+class Object
+  def fizzbuzz
+    return if !(self.is_a? Integer)
+
+    if self % 3 == 0 && self % 5 == 0
+      'FizzBuzz'
+    elsif self % 3 == 0
+      'Fizz'
+    elsif self % 5 == 0
+      'Buzz'
+    else
+      self
+    end
+
+  end
+end
+
+(1..20).each {|number| puts number.fizzbuzz}
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,26 +1,31 @@
 require 'fizzbuzz'
+require 'fizzbuzz_alt'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'Fizz'
+    expect(3.fizzbuzz).to eq 'Fizz'
   end
 end
 
 describe 'fizzbuzz' do
     it 'returns "buzz" when passed 5' do
         expect(fizzbuzz(5)).to eq 'Buzz'
+        expect(5.fizzbuzz).to eq 'Buzz'
     end
 end
 
 describe 'fizzbuzz' do
   it 'returns 4 when passed 4' do
     expect(fizzbuzz(4)).to eq 4
+    expect(4.fizzbuzz).to eq 4
   end
 end
 
 describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq "FizzBuzz"
+    expect(15.fizzbuzz).to eq "FizzBuzz"
   end
 end
 
@@ -28,6 +33,8 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed a multiple of 3' do
     expect(fizzbuzz(6)).to eq 'Fizz'
     expect(fizzbuzz(333)).to eq 'Fizz'
+    expect(6.fizzbuzz).to eq 'Fizz'
+    expect(333.fizzbuzz).to eq 'Fizz'
   end
 end
 
@@ -35,14 +42,19 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed a multiple of 5' do
     expect(fizzbuzz(10)).to eq 'Buzz'
     expect(fizzbuzz(20)).to eq 'Buzz'
+    expect(10.fizzbuzz).to eq 'Buzz'
+    expect(20.fizzbuzz).to eq 'Buzz'
   end
 end
 
 describe 'fizzbuzz' do
   it 'returns "nil" when passed anything which isn\'t an integer' do
     expect(fizzbuzz("string")).to eq nil
-    expect(fizzbuzz(1.5)).to eq nil
+    expect(fizzbuzz("1.5")).to eq nil
     expect(fizzbuzz(:symbol)).to eq nil
+    # expect("string".fizzbuzz).to eq nil
+    # expect(1.5.fizzbuzz).to eq nil
+    # expect(:symbol.fizzbuzz).to eq nil
   end
 end
 
@@ -51,6 +63,9 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(150)).to eq 'FizzBuzz'
     expect(fizzbuzz(1500)).to eq 'FizzBuzz'
     expect(fizzbuzz(45)).to eq 'FizzBuzz'
+    expect(150.fizzbuzz).to eq 'FizzBuzz'
+    expect(1500.fizzbuzz).to eq 'FizzBuzz'
+    expect(45.fizzbuzz).to eq 'FizzBuzz'
   end
 end
 
@@ -82,6 +97,34 @@ describe "Q1. FizzBuzz to 20" do
 
   it "`puts`es numbers 1 - 20, except Fizz if divisible by 3, Buzz if divisible by 5, FizzBuzz if divisible by 3 and 5" do
     expect { load "fizzbuzz.rb" }
+      .to output(/#{expected}$/).to_stdout
+  end
+end
+
+describe "Q1. FizzBuzz to 20" do
+  let(:expected) { ["1",
+                    "2",
+                    "Fizz",
+                    "4",
+                    "Buzz",
+                    "Fizz",
+                    "7",
+                    "8",
+                    "Fizz",
+                    "Buzz",
+                    "11",
+                    "Fizz",
+                    "13",
+                    "14",
+                    "FizzBuzz",
+                    "16",
+                    "17",
+                    "Fizz",
+                    "19",
+                    "Buzz"].join("\n") }
+
+  it "`puts`es numbers 1 - 20, except Fizz if divisible by 3, Buzz if divisible by 5, FizzBuzz if divisible by 3 and 5" do
+    expect { load "fizzbuzz_alt.rb" }
       .to output(/#{expected}$/).to_stdout
   end
 end
